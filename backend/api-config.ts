07ceb03b0f7ec38ca814791fb00cdbb29f582b44
Wagner Faria <wagnerfaria1601@gmail.com>
2017-11-24 16:35:31 -0300

    Aula 115 finalizada com sucesso

---
+++ backend/api-config.ts
@@ -0,0 +1,4 @@
+
+export const apiConfig = {
+    secret: 'meat-api-password'
+}
--- backend/users.ts
+++ backend/users.ts
@@ -5,7 +5,7 @@ export class User {
         return another !== undefined && another.email === this.email && another.password === this.password;
     }
 }
-export const users = {
+export const users: { [key: string]: User } = {
     "juliana@gmail.com": new User('juliana@gmail.com', 'Juliana', 'juliana23'),
     "amanda@gmail.com": new User('amanda@gmail.com', 'Amanda', 'amanda21')
 }
